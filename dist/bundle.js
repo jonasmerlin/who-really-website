681245e4b26ab3ec291e19cd4edab41b3413df11
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2017-12-10 15:53:25 +0100

    ajaxed everything

--- dist/bundle.js
+++ dist/bundle.js
@@ -76,45 +76,104 @@ var _greeter2 = _interopRequireDefault(_greeter);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log($('#urlToUpload')); /* eslint-disable no-console */
+var midPart = $('.mid-part'); /* eslint-disable no-console */
 
-console.log((0, _greeter2.default)());
+var forms = $('.forms');
 
-$('#urlButton').click(function (e) {
-  e.preventDefault();
-  // $.ajax({
-  //     type: "POST",
-  //     url: 'https://who-really.herokuapp.com/classification/portrait/url',
-  //     crossDomain:true,
-  //     success: function(data, status, xhr) {
-  //         console.log(data);;
-  //     }
-  // })
-  $.post("https://who-really.herokuapp.com/classification/portrait/url", $("#urlForm").serialize()).done(function (data) {
-    console.log(data);
+$('#fileForm').submit(function (event) {
+  event.preventDefault();
+
+  var $form = $(this);
+  var url = $form.attr("action");
+
+  forms.detach();
+
+  appendSpinnerTo(midPart);
+
+  // source: http://portfolio.planetjon.ca/2014/01/26/submit-file-input-via-ajax-jquery-easy-way/
+  var request = $.ajax({
+    url: url,
+    type: 'POST',
+    data: new FormData(this),
+    processData: false,
+    contentType: false
   });
-  replaceContent();
+
+  request.done(function (data) {
+    console.log(data);
+    midPart.empty();
+    appendPortraitDescriptionTo(midPart, data);
+  });
+
+  request.fail(function (data) {
+    midPart.empty();
+    forms.appendTo(midPart);
+    $(input).addClass('is-error');
+    $(inputContainer + ' .form-input-hint').remove();
+    appendInputHintTo($(inputContainer), data.responseJSON.error);
+  });
 });
 
-var replaceContent = function replaceContent() {
+$("#urlForm").submit(function (event) {
 
-  var midElement = $('.mid-part').empty(newBody);
-  var newBody = $("<div></div>").addClass('loading').on({
-    touchstart: function touchstart(event) {
-      // Do something
-    }
-  }).appendTo(midElement);
+  event.preventDefault();
 
-  setTimeout(function () {
-    midElement.empty();
-    var newBody = $("<div></div>", {
-      text: 'Go to Google!'
-    }).on({
-      touchstart: function touchstart(event) {
-        // Do something
-      }
-    }).appendTo(midElement).hide().fadeIn("slow");
-  }, 2000);
+  var $form = $(this);
+  var url = $form.attr("action");
+
+  forms.detach();
+
+  appendSpinnerTo(midPart);
+
+  var request = $.post(url, $form.serialize());
+
+  request.done(function (data) {
+    console.log(data);
+    midPart.empty();
+    appendPortraitDescriptionTo(midPart, data);
+  });
+
+  request.fail(function (data) {
+    midPart.empty();
+    forms.appendTo(midPart);
+    $('#urlInput').addClass('is-error');
+    $('#urlInputContainer .form-input-hint').remove();
+    appendInputHintTo($('#urlInputContainer'), data.responseJSON.error);
+  });
+});
+
+var appendPortraitDescriptionTo = function appendPortraitDescriptionTo(element, data) {
+  var text = 'This portrait is clearly of a ' + data.gender + ', ' + data.ethnicity + ' Person';
+  var description = $("<p></p>", {
+    text: text
+  });
+  description.appendTo(element);
+  var linkBack = $("<a></a>", {
+    text: "I'm offended, let me try again."
+  });
+  linkBack.click(function (e) {
+    restoreOriginalForm();
+  });
+  linkBack.appendTo(element);
+};
+
+var appendSpinnerTo = function appendSpinnerTo(element) {
+  var spinner = $("<div></div>");
+  spinner.addClass('loading');
+  spinner.appendTo(element);
+};
+
+var appendInputHintTo = function appendInputHintTo(element, text) {
+  var inputHint = $("<p></p>", {
+    text: text
+  });
+  inputHint.addClass('form-input-hint');
+  inputHint.appendTo(element);
+};
+
+var restoreOriginalForm = function restoreOriginalForm() {
+  midPart.empty();
+  midPart.append(forms);
 };
 
 /***/ }),
